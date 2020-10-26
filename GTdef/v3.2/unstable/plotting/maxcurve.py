8190d88fdd5e2a074bbaa36a7141ba0453a39ac6
Andrew Newman <anewman@gatech.edu>
2020-10-26 18:00:07 -0400

    Plotting fixes/updates. 1) GTdef_slipmodel now has option for only Horiz. or Vert. data visualization.  Too, I fixed an error where some point types were misread. 2) GTdef_misfit_kappa now has an improved optimization determinant. It's may not be possible to remove fully the range affect on the choice, but I've significantly improved what we're seeing with a new maximum curvature determination which looks at the change in the 2nd-order derivative relative to the 1st-ordrer.  These are normalized by the range of the competing parameter before calculations.  Optimal is plotted. 3) inclusion of small python scrypt to perform the maximum curvature calculation.

---
+++ GTdef/v3.2/unstable/plotting/maxcurve.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from numpy import max,min,abs,gradient
+import sys
+
+fin=sys.argv[1]
+
+#invout=pd.read_table('100kmchkbd_40kmstat_inv_inv.out',sep=r"\s+", header=1, names=['beta', 'kappa', 'data_num', 'slip_num', 'ndf', 'rss', 'rms', 'wrrs', 'wrms', 'chi2', 'rchi2', 'r1d', 'r2d', 'strain'])
+invout=pd.read_table(fin,sep=r"\s+", header=1, names=['beta', 'kappa', 'data_num', 'slip_num', 'ndf', 'rss', 'rms', 'wrrs', 'wrms', 'chi2', 'rchi2', 'r1d', 'r2d', 'strain'])
+
+x=invout.r2d.values
+y=invout.rms.values
+ynorm=y*(max(x)-min(x))
+xnorm=x*(max(y)-min(y))
+k=invout.kappa.values
+
+yp=gradient(ynorm,xnorm) #y-prime
+ypp=gradient(yp,xnorm) #y-dble prime
+curve=abs(ypp)/(1+yp**2)**1.5  # unsigned max curvature
+invout["curvature"]=curve
+
+imax=curve.argmax()
+
+invout_sm= invout[['kappa','r2d','rms','curvature']]
+print("# ",imax,k[imax],x[imax],y[imax], max(curve))
+print("# Curvature from rms/range(roughness) and roughness/range(rms) ")
+print("# ",invout_sm.columns.values)
+print(invout_sm.to_string(index=False,header=False))
+
+## interpolated solution
+#from scipy.interpolate import interp1d
+#from numpy import linspace
+# nx=100
+# f=interp1d(x,y,kind='cubic')
+# xi=linspace(min(x),max(x),num=nx, endpoint=True)
+# dx=(max(x)-min(x)/nx)
+# yi=f(xi)
+# yip=gradient(yi,dx)
+# yipp=gradient(yip,dx)
+
