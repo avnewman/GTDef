31adf63b1cb1db878844ea3938888bd8ccb9ded7
avnewman <andrewvnewman@gmail.com>
2021-01-22 12:40:59 -0500

    additional updates to maxcurve.py to avoid singularities, and commented lines

--- GTdef/v4.1/unstable/plotting/maxcurve.py
+++ GTdef/v4.1/unstable/plotting/maxcurve.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
-# v1.5 redone to normalize by own parameter, not oposite.
-# AVN 21 Jan 2021
+# v1.5.1 redone to normalize by own parameter, not oposite.
+# AVN 22 Jan 2021
+
 import pandas as pd
 from numpy import max,min,abs,gradient
+import numpy as np
 import sys
 
 fin=sys.argv[1]
 
 #invout=pd.read_table('100kmchkbd_40kmstat_inv_inv.out',sep=r"\s+", header=1, names=['beta', 'kappa', 'data_num', 'slip_num', 'ndf', 'rss', 'rms', 'wrrs', 'wrms', 'chi2', 'rchi2', 'r1d', 'r2d', 'strain'])
-invout=pd.read_table(fin,sep=r"\s+", header=1, names=['beta', 'kappa', 'data_num', 'slip_num', 'ndf', 'rss', 'rms', 'wrrs', 'wrms', 'chi2', 'rchi2', 'r1d', 'r2d', 'strain'])
+invout=pd.read_table(fin,sep=r"\s+", header=1, comment='#', names=['beta', 'kappa', 'data_num', 'slip_num', 'ndf', 'rss', 'rms', 'wrrs', 'wrms', 'chi2', 'rchi2', 'r1d', 'r2d', 'strain'])
+#invout=invout[ ~ invout['beta'].str.contains("#")] # remove lines that begin with "#" after reading header in read_table
+#get exponent of first rss value 
+rss0=int(abs(np.floor(np.log10(invout.loc[0,'rss']))))
+dp=rss0+2 # add to more decimal places to get final
+#print(dp)
+invout['rss']=invout[['rss']].round(decimals=dp) # round rss to desired dp
+invout=invout.drop_duplicates(subset=['rss'],keep='last') # removes duplicates in rss
+#print(invout['rss'])
 
 x=invout.r2d.values
 y=invout.rms.values
@@ -20,9 +30,11 @@ ynorm=y/(max(y)-min(y))
 xnorm=x/(max(x)-min(x))
 k=invout.kappa.values
 
+
 yp=gradient(ynorm,xnorm) #y-prime
 ypp=gradient(yp,xnorm) #y-dble prime
-curve=abs(ypp)/(1+yp**2)**1.5  # unsigned max curvature
+curve=np.true_divide(abs(ypp),(1+yp**2)**1.5)  # unsigned max curvature
+curve[ ~ np.isfinite(curve)]=0  # set singularities to zero.
 invout["curvature"]=curve
 
 imax=curve.argmax()
@@ -43,3 +55,4 @@ print(invout_sm.to_string(index=False,header=False))
 # yi=f(xi)
 # yip=gradient(yi,dx)
 # yipp=gradient(yip,dx)
+
